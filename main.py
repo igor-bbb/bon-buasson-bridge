4ee31cff4a76f0c60afc40ec6d0f8c22f0f4a961
igor-bbb <miv1979@ukr.net>
2026-03-21 16:49:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Проверка что API живо
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-# Тестовый endpoint
 @app.get("/test")
 def test():
     return {"message": "Bon Buasson API работает"}
 
-# DATA endpoint (пока простой)
 @app.get("/data")
 def get_data():
     return {"message": "DATA endpoint works"}
