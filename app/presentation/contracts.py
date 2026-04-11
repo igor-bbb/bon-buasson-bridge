0aac004bcbd4b5ad9cab1fbd5a98936e19308cf1
igor-bbb <miv1979@ukr.net>
2026-04-11 18:37:09 +0300

    Update contracts.py

--- app/presentation/contracts.py
+++ app/presentation/contracts.py
@@ -3,24 +3,24 @@ from typing import Any, Dict, Optional
 
 def ok_response(query: Optional[Dict[str, Any]], data: Dict[str, Any]) -> Dict[str, Any]:
     return {
-        'status': 'ok',
-        'query': query or {},
-        'data': data,
+        "status": "ok",
+        "query": query or {},
+        "data": data,
     }
 
 
 def error_response(message: str, query: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
     return {
-        'status': 'error',
-        'reason': message,
-        'query': query or {},
+        "status": "error",
+        "reason": message,
+        "query": query or {},
     }
 
 
 def not_implemented_response(query: Optional[Dict[str, Any]], message: str) -> Dict[str, Any]:
     return {
-        'status': 'error',
-        'reason': message,
-        'query': query or {},
-        'not_implemented': True,
+        "status": "error",
+        "reason": message,
+        "query": query or {},
+        "not_implemented": True,
     }
